cfbc7147b96b3d9297e32c8b518dc5a3f9701f19
dzc <duzhicai@antiy.cn>
2018-09-28 13:49:21 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrap">
+        <p class="message"> Hello World</p>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HelloWorld",
+  components: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+    width: 100%;
+    height: 100%;
+    .message {
+        color: #ff0000;
+    }
+}
+</style>
+
+
